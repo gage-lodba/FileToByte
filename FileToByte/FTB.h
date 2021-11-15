9cd03fa7a1a2919c2694126137f7c501945c0ffa
Jerimiah <38990407+JerimiahOfficial@users.noreply.github.com>
2021-11-15 15:20:20 -0500

    Add files via upload

---
+++ FileToByte/FTB.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <filesystem>
+#include <future>
+
+namespace FileToByte {
+	extern void Convert(std::string val);
+}
